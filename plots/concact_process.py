e84eb00b35c79daeb9dca8ed236f58ae78817e6b
stupidpig <543825440@qq.com>
2023-05-05 00:11:48 -0700

    plot

--- plots/concact_process.py
+++ plots/concact_process.py
@@ -18,12 +18,4 @@ for i in range(1,len(files)):
     df1 = pd.concat([df1, df2])
 
 
-# df1 = pd.read_csv('part7.csv')
-# df2 = pd.read_csv('events.out.tfevents.1682858781.iGpu25.csv')
-#
-# # add 1000000 to the step column of the second file
-# df2['step'] += 80000000
-#
-# # concatenate the two dataframes
-# result = pd.concat([df1, df2])
 df1.to_csv('final.csv',index=False)
